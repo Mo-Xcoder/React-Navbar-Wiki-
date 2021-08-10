0bce03c06e49082cd983310b6e20f100d3232f4f
ifreezeu1 <ifreezeu2@gmail.com>
2021-08-10 11:03:31 +0100

    created repo

---
+++ jojo_wiki/src/components/SideBarData.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import BTimg from '../images/BT.jpg'
+
+export const SideBarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text',
+    },
+    {
+        title: 'Phantom Blood',
+        path: '/PB',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text',
+    },
+    {
+        title: 'Battle Tendancy',
+        path: '/BT',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text',
+    },
+    {
+        title: 'Stardust Crusaders',
+        path: '/SC',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text',
+    },
+]
---
+++ jojo_wiki/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className='home'>
+            <h1 className="Title">Home of the JoJo Wiki</h1>
+        </div>
+    )
+}
+
+export default Home
